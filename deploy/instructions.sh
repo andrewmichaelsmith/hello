798dc0e04c9ed90ee93af1252d700711767e5053
Andrew Smith <me@andrewmichaelsmith.com>
2017-07-23 11:47:07 +0100

    nltk data download

--- deploy/instructions.sh
+++ deploy/instructions.sh
@@ -1,11 +1,14 @@
+apt-get update
 apt-get install -y hostapd dnsmasq nginx python3-pip
 /etc/init.d/nginx stop
 
 cd /opt/hello
 pip3 install -r requirements.txt
-python3 -m nltk.downloader -d ~/nltk_data wordnet punkt stopwords vader_lexicon
+
 systemctl enable hello
 
 systemctl enable ssh
 
+mkdir -p /opt/nltk_data
+python3 -m nltk.downloader -d /opt/nltk_data wordnet punkt stopwords vader_lexicon
 
