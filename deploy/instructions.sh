dff7c895ce3b0aae97697ad9700d35bf76f3ad66
Andrew Smith <me@andrewmichaelsmith.com>
2017-07-23 11:14:31 +0100

    Install ntlk data

--- deploy/instructions.sh
+++ deploy/instructions.sh
@@ -3,6 +3,7 @@ apt-get install -y hostapd dnsmasq nginx python3-pip
 
 cd /opt/hello
 pip3 install -r requirements.txt
+python3 -m nltk.downloader -d ~/nltk_data wordnet punkt stopwords vader_lexicon
 systemctl enable hello
 
 systemctl enable ssh
